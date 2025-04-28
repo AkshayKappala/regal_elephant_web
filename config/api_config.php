c37a6feb3c1125895fbd6589440a410fb5a1ec8e
Akshay Kappala <39707488+AkshayKappala@users.noreply.github.com>
2025-04-27 21:42:01 -0500

    Refactor base URL generation to include production check for improved security and protocol handling.

--- config/api_config.php
+++ config/api_config.php
@@ -1,6 +1,14 @@
 <?php
 // Generate base URL dynamically based on server information
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+$isProduction = (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'ondigitalocean.app') !== false);
+
+// Force HTTPS on production, otherwise detect protocol
+if ($isProduction) {
+    $protocol = 'https://';
+} else {
+    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+}
+
 $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
 $baseDir = dirname($_SERVER['PHP_SELF'], 3); // Go up three levels from config dir
 $baseUrl = $protocol . $host . $baseDir;
