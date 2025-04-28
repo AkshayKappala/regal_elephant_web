5da80c1a44b90869cd6487bb1e04ec707f1f9260
Akshay Kappala <39707488+AkshayKappala@users.noreply.github.com>
2025-04-27 21:37:12 -0500

    Refactor API URL definition to generate base URL dynamically based on server information for improved flexibility.

--- config/api_config.php
+++ config/api_config.php
@@ -1,5 +1,12 @@
 <?php
-define('STAFF_API_URL', 'http://localhost/INFS730/regal_elephant_web/staff/api/');
+// Generate base URL dynamically based on server information
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+$host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
+$baseDir = dirname($_SERVER['PHP_SELF'], 3); // Go up three levels from config dir
+$baseUrl = $protocol . $host . $baseDir;
+
+// Define API URLs dynamically
+define('STAFF_API_URL', $baseUrl . '/staff/api/');
 
 define('API_KEY', 'regal_elephant_secure_api_key');
 
